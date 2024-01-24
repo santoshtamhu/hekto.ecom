73b55e295246cd7fdebce8867ed59ebeaa41d064
santoshtamhu <187sang@gmail.com>
2024-01-24 17:50:48 +0545

    protected login page from logged in users

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import LoginForm from "../components/common/LoginForm";
 import { Breadcrumb } from "../components/common/Breadcrumb";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const LoginPage = () => {
+  const user = useSelector((store) => store.user.value);
+  if (user) {
+    return <Navigate to="/" />;
+  }
   return (
     <>
       <Breadcrumb
