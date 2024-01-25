af53ab99962d24ba45a14f98fcfde1a9e147116f
santoshtamhu <187sang@gmail.com>
2024-01-25 14:55:47 +0545

    login popup fix

--- src/components/common/LoginPopUp.jsx
+++ src/components/common/LoginPopUp.jsx
@@ -4,16 +4,21 @@ import LoginForm from "./LoginForm";
 export const LoginPopUp = ({ onClose }) => {
   return (
     <>
-      <div className="fixed left-1/2 top-1/2 z-20 h-[474px] -translate-x-1/2 -translate-y-1/2 transform bg-white">
-        <div className="relative ">
-          <LoginForm />
-          <button
-            className="absolute left-[510px] top-4
-             z-30 font-lato text-lg text-slate-500 hover:text-black"
-            onClick={onClose}
-          >
-            X
-          </button>
+      <div
+        onClick={onClose}
+        className="fixed left-1/2 top-1/2 z-20 flex h-full w-full -translate-x-1/2 -translate-y-1/2 transform items-center justify-center bg-transparent"
+      >
+        <div className="h-[474px] bg-white">
+          <div className="relative ">
+            <LoginForm />
+            <button
+              className="absolute left-[510px] top-4
+            z-30 font-lato text-lg text-slate-500 hover:text-black"
+              onClick={onClose}
+            >
+              X
+            </button>
+          </div>
         </div>
       </div>
     </>
