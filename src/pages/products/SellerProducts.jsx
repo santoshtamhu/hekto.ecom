1630b985a7da9d1175a33a0cdb1b876ed3093723
santoshtamhu <187sang@gmail.com>
2024-01-25 21:28:17 +0545

    seller products

--- src/pages/products/SellerProducts.jsx
+++ src/pages/products/SellerProducts.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { Breadcrumb } from "../../components/common/Breadcrumb";
+
+import { Link } from "react-router-dom";
 
 export const SellerProducts = () => {
   return (
@@ -19,13 +20,20 @@ export const SellerProducts = () => {
               <th class="border-b px-4 py-2">Name</th>
               <th class="border-b px-4 py-2">Price</th>
               <th class="border-b px-4 py-2">In Stock</th>
+              <th class="border-b px-4 py-2">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr className="text-center">
               <td class="border-b px-4 py-2">Nike Chair</td>
               <td class="border-b px-4 py-2">25</td>
               <td class="border-b px-4 py-2">200</td>
+              <td class=" flex justify-center gap-4 border-b px-4 py-2 text-center">
+                <Link>
+                  <span>Edit</span>
+                </Link>
+                <button>Delete</button>
+              </td>
             </tr>
           </tbody>
         </table>
